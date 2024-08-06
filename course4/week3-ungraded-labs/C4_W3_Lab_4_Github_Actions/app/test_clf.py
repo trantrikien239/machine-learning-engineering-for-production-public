3349e911385ba756ecf1061bc1a752d4c4313385
Kien Tran <trantrikien239@gmail.com>
2024-08-06 11:40:26 -0400

    Fix clf-95 by adding scaling to the pipeline

--- course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/test_clf.py
+++ course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/test_clf.py
@@ -1,5 +1,7 @@
 import pickle
 from main import clf
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
 
 
 def test_accuracy():
@@ -16,3 +18,11 @@ def test_accuracy():
 
     # Accuracy should be over 90%
     assert acc > 0.9
+
+def test_pipeline_and_scaler():
+    isPipeline = isinstance(clf, Pipeline)
+    assert isPipeline
+
+    if isPipeline:
+        first_step = [v for v in clf.named_steps.values()][0]
+        assert isinstance(first_step, StandardScaler)
